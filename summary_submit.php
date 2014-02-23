f7cf7b4a5ffea3e5c95f6a8434f3c0c16af260db
Ali Nahm <alisnahm@gmail.com>
2014-02-22 23:14:17 -0500

    daily summary form

---
+++ summary_submit.php
@@ -0,0 +1,108 @@
+<?php
+
+require("phpmailer/class.phpmailer.php");
+require("phpmailer/class.smtp.php");
+
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Host = 'smtp.fas.harvard.edu';
+
+
+//Retrieve Variables from POST
+
+
+//General Variables
+$group = $_POST['Group'] ;
+$day = $_POST['Day'] ;
+$date = $_POST['Date'] ;
+$author = $_POST['Author'] ;
+
+//Group Summary Variables
+$kids = $_POST['Kids'] ;
+$counselors = $_POST['Counselors'] ;
+$meeting = $_POST['Meeting'] ;
+$homework = $_POST['Homework'] ;
+$project = $_POST['Project'] ;
+$pickup = $_POST['Pickup'] ;
+$goodbad = $_POST['ProbsGood'] ;
+$comments = $_POST['Comments'];
+
+//Project Card Variables
+$title = $_POST['Title'] ;
+$topic = $_POST['Topic'] ;
+$other = $_POST['Other'] ;
+$series = $_POST['Series'] ;
+$inspiration = $_POST['Inspiration'] ;
+$preparation = $_POST['Preparation'] ;
+$materials = $_POST['Materials'] ;
+$procedures = $_POST['Procedures'] ;
+$results = $_POST['Results'] ;
+$comments3 = $_POST['Comments3'] ;
+
+//write summary to e-mail lists
+
+$report .= "$group summary for $day, $date" ;
+$report = "\nwritten by: $author" ;
+$report .= "\n\n\t\t ATTENDANCE" ;
+$report .= "\n Kids present: $kids" ;
+$report .= "\n Counselors present: $counselors" ;
+$report .= "\n\n\t\t MEETING KIDS" ;
+$report .= "\n $meeting" ;
+$report .= "\n\n\t\t HOMEWORK" ;
+$report .= "\n $homework" ;
+$report .= "\n\n\t\t PROJECT TIME" ;
+$report .= "\n $project";
+$report .= "\n\n\t\t PICK UP" ;
+$report .= "\n $pickup" ;
+$report .= "\n\n\t\t PROBLEMS & GOOD STUFF" ;
+$report .= "\n $goodbad" ;
+$report .= "\n\n\t\t COMMENTS, SUGGESTIONS" ;
+$report .= "\n $comments" ;
+
+//mail to lists
+
+$mail->SetFrom('anahm@outlook.com');
+
+$mail->AddAddress('anahm@mit.edu');
+$mail->Subject = 'blargl';
+$mail->Body = 'hi my name is ali, sup';
+
+if(!$mail->send()) {
+    echo 'blargl life';
+    echo $mail->ErrorInfo;
+}
+
+
+
+// $to = "chtnasp-$group" ;
+// $to .= "@lists.hcs.harvard.edu" ;
+$to = "alison.nahm@gmail.com";
+
+$re = "[chtnasp] $group report for $day, $date" ;
+$msg = $report;
+
+mail ($to, $re, $msg);
+
+?>
+
+<html>
+
+<head>
+<title> Group Summary and Project Cards</title>
+</head>
+
+<body bgcolor=990000>
+
+<font color=white>
+
+<h1>Group Summary was sent!!</h1>
+
+Thank you, <?php echo ( $author ); ?>, for submitting!
+<BR>
+<BR>Return <a href="http://www.hcs.harvard.edu/~chtnasp">home</a>
+
+</body>
+
+
+</html>
+
